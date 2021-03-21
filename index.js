54ed2f240423d5fc59f45226af6e221870bb4ed3
Jeremy Hayes <dev@jeremyhayes.net>
2021-03-21 17:01:15 -0400

    add login/status to device
    Had to rearrange imports to make this an ES module.

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 #!/usr/bin/env node
 
-const yargs = require('yargs');
+import yargs from 'yargs';
+import { HnapClient } from 'motorola-mb8600-client/client.js';
 
-const options = yargs
-    .option('url', { default: 'https://192.168.100.1' })
+const options = yargs(process.argv)
+    .option('url', { default: 'http://192.168.100.1' })
     .option('s', { alias: 'ignore-ssl', default: false, type: 'boolean' })
     .option('u', { alias: 'username', default: 'admin' })
     .option('p', { alias: 'password', default: 'motorola' })
     .argv;
 
-console.log(options);
+const client = new HnapClient(options.url, options.ignoreSsl);
+
+(async () => {
+    const loginResult = await client.login(options.username, options.password);
+    console.log(loginResult);
+
+    const connectionInfoResult = await client.getConnectionInfo();
+    console.log(connectionInfoResult);
+})();
